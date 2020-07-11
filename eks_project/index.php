1631bfd70ff7a7f0e9edc271b01274a7aa1dac14
Akash-droid24 <64472357+Akash-droid24@users.noreply.github.com>
2020-07-11 19:29:28 +0530

    mycommit

---
+++ eks_project/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+div {
+    height: 400px;
+    background: linear-gradient(to bottom right, #6699ff 0%, #ff99cc 100%)
+}
+<?php 
+
+echo "Hello World !!:-)<br>";
+echo "I'm loving EKS Services<br>";
+
+?>
+</style>
+<div></div>
+</body>
+</html>
